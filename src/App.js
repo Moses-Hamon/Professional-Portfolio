68f7f240798c5ae543f08366c34781347965ac4d
Moses Hamon <moseslhamon@gmail.com>
2020-01-29 19:52:59 +1000

    added data for about me

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import TopBar from './components/TopBar';
+import AboutMe from "./components/AboutMe"
 import './App.css';
 
 function App() {
   return (
-    <div className="App flex">
+    <div className="App flex-col">
     <TopBar />
+    <AboutMe />
     </div>
   );
 }
--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -3,6 +3,16 @@ import React, { Component } from "react";
 import Section from "./Section";
 
 const aboutMeData = {
+  img: "https://avatars0.githubusercontent.com/u/41456505?s=400&v=4",
+  url: "https://www.linkedin.com/in/moses-hamon/",
+  blurb: {
+    Background:
+      "I recently decided to transition to a career in Software Development, I have completed a Diploma Of Software Development. I've enjoyed developing my skills in the Web Development space, gaining professional experience within the startup arena and making friends with other like-minded developers.",
+    Hobbies:
+      "Developing, mountain biking, online gaming, fishing, camping.",
+    Goals:
+      "Work hard and expand on my software knowledge and skills as quickly as possible."
+  }
 };
 
 const AboutMeBlurb = props => {
@@ -25,8 +35,12 @@ class AboutMe extends Component {
   render() {
     const { img, url, blurb } = aboutMeData;
     return (
+      <>
       <Section heading="About Me">
+        
       </Section>
+      <AboutMeBlurb blurb={blurb} />
+      </>
     );
   }
 }
